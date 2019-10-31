5bb37ba0e171540cad8da0d9748014a2e69a6595
mpalitto <mpalitto@gmail.com>
2019-10-31 18:46:01 +0200

    Add files via upload

---
+++ CurrentDrawMonitor/Arduino/current_draw_mqtt_esp8266.ino
@@ -0,0 +1,108 @@
+/*
+NodeMCU used as current draw monitor
+A0 is the analog input
+on board LED blinking to show it is not dead
+Connect to WIFI network
+and it publishes to MQTT server the values
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#define LED_BUILTIN 2
+
+// Update these with values suitable for your network.
+const char* ssid = "PALITTO-BONCRISTIANO";
+const char* password = "scalaEapt11";
+
+WiFiClient espClient;
+long lastMsg = 0;
+// String msg;
+char msg[75];
+// int value = 0;
+int current = 0;
+int led = 0;
+int maxx = 0;
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  int led=0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (led == 0) {
+      digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+      led = 1;
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+      led = 0;
+    }
+  digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+//---------------------
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, 75, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }
+}
+
+//---------------------
+
