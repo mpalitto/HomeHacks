45550b35af0dba0cff47b507ed73f9853e94576d
mpalitto <mpalitto@gmail.com>
2019-11-01 19:33:57 +0200

    Add files via upload

--- sONOFFserver/iptables.rules.sh
+++ sONOFFserver/iptables.rules.sh
@@ -1,2 +1,3 @@
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -t nat -I PREROUTING -i enxc4e9840b09a5 -p tcp --dport 443 -j REDIRECT --to-port 8080
+iptables-save > /etc/network/iptables.rules
